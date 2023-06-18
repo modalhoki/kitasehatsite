7d9822963886541adc175330f10da1b7ec27920a
modalhoki <72333392+modalhoki@users.noreply.github.com>
2023-06-18 18:17:07 +0700

    rename fasilitas_rumah_sakit fields

--- src/config.production.php
+++ src/config.production.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "z64Y6Y523e6vCWu3", // API Secret Key
+        "SECRET_KEY" => "n668rG2xQFz8ej5W", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
